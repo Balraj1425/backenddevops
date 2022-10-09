3a5f294b933fa6cec5302146361c6abd20900ee9
Balraj <bhullar.bs@gmail.com>
2022-10-09 11:29:53 +0530

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+const cors = require('cors')
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 const userroutes =  require('./routes/user')
@@ -8,15 +9,14 @@ const userroutes =  require('./routes/user')
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
+app.use(cors())
 
 
 app.get("/",(req,res)=>{
-
     res.send("it works!!")
-
 })
 
 app.use('/data', userroutes)
 
   
-app.listen(3000,() => {console.log("server started on port 3000")});
+app.listen(3001,() => {console.log("server started on port 3001")});
